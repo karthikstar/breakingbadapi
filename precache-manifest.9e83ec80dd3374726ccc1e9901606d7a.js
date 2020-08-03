d7b293c8013e5b82b49a6dc8e903af7e43b99a5d
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2020-08-03 22:46:57 +0800

    Updates

---
+++ precache-manifest.9e83ec80dd3374726ccc1e9901606d7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af55c57692e6f33e6e50b1f98026632e",
+    "url": "/breakingbadapi/index.html"
+  },
+  {
+    "revision": "69cf26b7d80ae8ff8393",
+    "url": "/breakingbadapi/static/css/main.d45720bb.chunk.css"
+  },
+  {
+    "revision": "33f97e6b2b60781c3e5e",
+    "url": "/breakingbadapi/static/js/2.a2730ed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbadapi/static/js/2.a2730ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69cf26b7d80ae8ff8393",
+    "url": "/breakingbadapi/static/js/main.ebc0f5ad.chunk.js"
+  },
+  {
+    "revision": "bcf649c47cd51ff35a14",
+    "url": "/breakingbadapi/static/js/runtime-main.3e75ba79.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakingbadapi/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingbadapi/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breakingbadapi/static/media/spinner.235b9b3e.gif"
+  }
+]);
